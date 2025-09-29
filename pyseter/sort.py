bc382db60c39ae99ac0ff64c6da7f26b6002c2c9
Philip Patton <pattonp@hawaii.edu>
2025-09-29 14:12:23 -0400

    sort should work with non jpgs too

--- pyseter/sort.py
+++ pyseter/sort.py
@@ -34,7 +34,7 @@ def process_images(image_root: str, all_image_dir: str) -> Tuple[List[str], List
         dirs[:] = [d for d in dirs if d not in all_image_dir]
         dirs[:] = [d for d in dirs if 'cluster' not in d]
         for file in files:
-            if not file.lower().endswith('.jpg'):
+            if not file.lower().endswith(('.jpg', '.jpeg', '.png')):
                 continue
             
             image_list.append(file)
