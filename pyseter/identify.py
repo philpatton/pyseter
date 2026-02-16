25019300a0f55a7910996e821c6db836da28943a
philpatton <philtpatton@gmail.com>
2026-02-16 13:52:36 -0500

    add update_reference_features

--- pyseter/identify.py
+++ pyseter/identify.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import pandas as pd
 
-def _find_neighbors(ref_feat, query_feat):
+def find_neighbors(ref_feat, query_feat):
     """Find the most similar images to the reference set to the query set."""
     neighborhood = NearestNeighbors(n_neighbors=500, metric="cosine")
     neighborhood.fit(ref_feat)
@@ -12,7 +12,7 @@ def _find_neighbors(ref_feat, query_feat):
     distances, indices = neighborhood.kneighbors(query_feat)
     return distances, indices
 
-def _insert_new_id(distances, predictions, threshold=0.5):
+def insert_new_id(distances, predictions, threshold=0.5):
     """Insert new_individual into the predictions at the threshold."""
 
     # find position where we should slot new individual
@@ -28,7 +28,7 @@ def _insert_new_id(distances, predictions, threshold=0.5):
 
     return new_dist, new_preds
 
-def _pool_predictions(predictions, distances):
+def pool_predictions(predictions, distances):
     """Remove redundant predictions, i.e., those of the same individual."""
 
     # find position of unique entry to eliminate redundant predictions 
@@ -91,16 +91,16 @@ def predict_ids(reference_dict, query_dict, id_df, proposed_id_count=10):
     ids = id_df.set_index('image').loc[reference_files, 'individual_id'].to_numpy()
 
     # takes about 19 seconds
-    distance_matrix, index_matrix = _find_neighbors(reference_feats, query_feats)
+    distance_matrix, index_matrix = find_neighbors(reference_feats, query_feats)
 
     # get the corresponding labels for each reference image
     predicted_ids = ids[index_matrix]
 
     # insert the prediction "new_individual" at the threshold
-    distances, ids = _insert_new_id(distance_matrix, predicted_ids, threshold=0.5)
+    distances, ids = insert_new_id(distance_matrix, predicted_ids, threshold=0.5)
 
     # remove redundant predictions and take the maximum 
-    pooled_distances, pooled_ids = _pool_predictions(ids, distances)
+    pooled_distances, pooled_ids = pool_predictions(ids, distances)
 
     final_predictions = [t[:proposed_id_count].tolist() for t in pooled_ids]
     final_distances = [t[:proposed_id_count].tolist() for t in pooled_distances]
@@ -120,3 +120,44 @@ def predict_ids(reference_dict, query_dict, id_df, proposed_id_count=10):
     pred_df['rank'] = pred_df['rank'] + 1
 
     return pred_df
+
+def update_reference_features(reference_dict, query_dict, confirmed_id_df):
+    """Update the reference feature dict with confirmed matches
+
+    Return a dictionary where the keys are the updated reference images and the
+    values contain the updated feature array.
+
+    Parameters
+    ----------
+    reference_dict : dict
+        Dictionary where the key is the reference image's name. The value 
+        associate with each key is a NumPy array of shape (M, ) where M is the 
+        number of features in the feature vector.
+    query_dict : dict
+        Dictionary where the key is the query image's name. The value 
+        associate with each key is a NumPy array of shape (M, ) where M is the 
+        number of features in the feature vector.
+    id_df : pd.DataFrame
+        DataFrame containing the identities, `individual_id`, and image file 
+        name, `image`, for every image in the reference set.
+    proposed_id_count : integer
+        The number of proposed IDs to return for each query image.
+    """
+    
+    # reorder the query_dict to make it match the order of the confirmed_match_df
+    confirmed_images = confirmed_id_df.image
+    confirmed_feature_dict = {i: query_dict[i] for i in confirmed_images}
+
+    # unpack both dicts 
+    reference_features = np.array(list(reference_dict.values()))
+    confirmed_features = np.array(list(confirmed_feature_dict.values()))
+
+    # stack the reference features
+    updated_features = np.vstack((reference_features, confirmed_features))
+
+    # new dict
+    reference_images = np.array(list(reference_dict.keys()))
+    updated_images = np.concatenate((reference_images, confirmed_images))
+    updated_dict = dict(zip(updated_images, updated_features))
+
+    return updated_dict
